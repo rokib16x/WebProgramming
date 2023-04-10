efce0069eee60880b2055163ed5d98715612fd12
Rokib <rokib16x@gmail.com>
2023-04-10 14:45:43 +0600

    pine

--- classFour/pineapple/index.php
+++ classFour/pineapple/index.php
@@ -6,7 +6,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PineApple</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style2.css">
 </head>
 
 <body>
@@ -14,31 +14,31 @@
         <table>
             <tr>
                 <td>Enter the number of pineapple:</td>
-                <td><input type="text" name="user_name"></td>
+                <td><input type="text" name="number"></td>
             </tr>
             <tr>
                 <td>Home Delivery:</td>
-                <td><select name="user_address">
-                    <option value="home">Yes</option>
-                    <option value="work">No</option>
+                <td><select name="delivery_option">
+                    <option value="yes">Yes</option>
+                    <option value="no">No</option>
                 </select>
                 </td>
             </tr>
             <tr>
                 <td>Tip:</td>
-                <td><input type="text" name="user_name"></td>
+                <td><input type="text" name="tip"></td>
             </tr>
             <tr>
                 <td>Extra Salt:</td>
-                <td><select name="user_address">
-                    <option value="home">Small</option>
-                    <option value="work">Medium</option>
-                    <option value="add">Large</option>
+                <td><select name="extra_salt">
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
                 </select>
                 </td>
             </tr>
             <tr>
-                <td colspan="2" align="center"> <input type="submit" value="Submit">
+                <td colspan="2" align="center"> <input type="submit" value="Generate Bill">
                 <td>
             </tr>
         </table>
