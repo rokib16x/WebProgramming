4b3a38176119d0d8d145339a590140b7bd0312cc
Rokib <rokib16x@gmail.com>
2023-04-10 14:03:37 +0600

    us

---
+++ classFour/basic/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="process.php" method="post">
+    <table>
+        <tr><td>Name:</td><td><input type="text" name="user_name"></td></tr>
+        <tr><td>Address:</td><td><textarea  name="user_address"></textarea></td></tr>
+        <tr><td colspan="2" align="center"> <input type="submit" value="Submit"><td></tr>
+    </table>
+    </form>
+</body>
+</html>
---
+++ classFour/basic/process.php
@@ -0,0 +1,9 @@
+<?php 
+
+$name =$_POST["user_name"];
+
+$address =$_POST["user_address"];
+
+echo $name."---".$address;
+
+?>
